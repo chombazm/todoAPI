50219fc3f536a6e4b64f4aac0f123f79a5674d92
Chombazm <dev.chomba@gmail.com>
2021-07-12 05:21:29 +0200

    Basis API Scafolding - Id, Name

---
+++ todoApp/Models/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isCompleted { get; set; }
+
+
+        //public string date_added { get; set; }
+
+
+
+
+    }
+}
